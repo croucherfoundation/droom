d768b7da6fdf4918113b580b191e6f3ae509cbc1
Zin Moe <dev.zinmoe@gmail.com>
2022-08-22 14:22:26 +0630

    redirect to root-url if cookie valid

--- app/controllers/droom/users/sessions_controller.rb
+++ app/controllers/droom/users/sessions_controller.rb
@@ -1,16 +1,22 @@
+require 'droom/auth_cookie'
+
 module Droom::Users
   class SessionsController < Devise::SessionsController
     before_action :set_access_control_headers
     skip_before_action :verify_authenticity_token, raise: false
 
     def new
-      if user_signed_in? && session['user_return_to'] == '/users/sign_in'
-        redirect_to root_path
-      end
-      if @page = Droom::Page.published.find_by(slug: "_welcome")
-        render template: "droom/pages/published", layout: Droom.page_layout
+      cookie = Droom::AuthCookie.new(cookies)
+      if cookie.valid? && cookie.fresh?
+        @user = Droom::User.find_by(unique_session_id: cookie.token)
+        sign_in(@user)
+        redirect_to '/'
       else
-        super
+        if @page = Droom::Page.published.find_by(slug: "_welcome")
+          render template: "droom/pages/published", layout: Droom.page_layout
+        else
+          super
+        end
       end
     end
 
