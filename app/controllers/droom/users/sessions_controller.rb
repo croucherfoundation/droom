ed2762ecfcd0326a73a31fef5c1ef4e8e7a41638
Zin Moe <dev.zinmoe@gmail.com>
2022-08-24 16:21:32 +0630

    check session present

--- app/controllers/droom/users/sessions_controller.rb
+++ app/controllers/droom/users/sessions_controller.rb
@@ -7,12 +7,14 @@ module Droom::Users
 
     def new
       cookie = Droom::AuthCookie.new(cookies)
-      if cookie.valid? && cookie.fresh?
+      if cookie.valid? && cookie.fresh? && session['warden.user.user.key'].present?
         @user = Droom::User.find_by(unique_session_id: cookie.token)
         sign_in(@user)
-        session['flash']['flashes']['alert'] = 'You are already signed in!' if session['flash']
+        session['flash']['flashes']['alert'] = 'You are already signed in!' if session['flash'] && session['flash']['flashes']
         redirect_to '/'
       else
+        current_user.clear_session_ids! if current_user
+        Droom::AuthCookie.new(warden.cookies).unset
         if @page = Droom::Page.published.find_by(slug: "_welcome")
           render template: "droom/pages/published", layout: Droom.page_layout
         else
