fb6b4e45594cc3aeda6a691d37bcd939d1b44680
Zin Moe <dev.zinmoe@gmail.com>
2023-04-03 14:54:16 +0630

    fixed document sorting

--- app/models/droom/document.rb
+++ app/models/droom/document.rb
@@ -14,6 +14,7 @@ module Droom
 
     before_create :inherit_confidentiality
     before_save :set_file_path_and_event
+    after_commit :bump_same_position
 
     # validates :file, :presence => true
     # do_not_validate_attachment_file_type :file
@@ -260,6 +261,14 @@ module Droom
         attachment.close
       else
         file.instance_write :file_name, name
+      end
+    end
+
+     def bump_same_position
+      documents = folder.documents.where("position = ? AND id != ?", position, id)
+      documents.each_with_index do |d, i|
+        d.update_column(:position, (d.position + i + 1))
+        puts "Bump document #{d.id} position to #{d.position}!"
       end
     end
   end
