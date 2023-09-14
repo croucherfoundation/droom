ae0610eda6162596ca50002c15c770b71f4d81a5
Thiha <thiha.dve@gmail.com>
2023-09-14 15:59:54 +0630

    cors remove

--- config/initializers/cors.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV['CORS_ORIGINS']
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete],
-    credentials: true
-  end
-end
