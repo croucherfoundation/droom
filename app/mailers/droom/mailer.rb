4974fab198f281035813485ffc6b74d74f36d56b
Thiha <thiha.dve@gmail.com>
2023-10-13 15:59:58 +0630

    bcc eamil add

--- app/mailers/droom/mailer.rb
+++ app/mailers/droom/mailer.rb
@@ -1,6 +1,7 @@
 module Droom
   class Mailer < ActionMailer::Base
     layout Droom.email_layout
+    before_action :bcc_email
     default from: '"Hong Kong Foundations Exchange" <info@hkfx.org>'
 
     def org_confirmation(organisation)
@@ -8,7 +9,7 @@ module Droom
       @user = organisation.owner
       @subject = I18n.t("registration.confirmation_subject", name: organisation.name)
       @email = Rails.env.production? ? @user.email : Settings.email.sandbox
-      mail(to: @email, subject: @subject)
+      mail(to: @email, bcc: bcc_email, subject: @subject)
     end
 
     def org_notification(organisation, admin, noti_type)
@@ -18,7 +19,7 @@ module Droom
       @noti_type = noti_type
       @subject = @noti_type == :created ? I18n.t("registration.notification_subject", name: organisation.name) : I18n.t("withdraw.notification_subject")
       @email = Rails.env.production? ? @admin.email : Settings.email.sandbox
-      mail(to: @email, subject: @subject)
+      mail(to: @email, bcc: bcc_email,subject: @subject)
     end
 
     def org_welcome(organisation, token)
@@ -27,7 +28,13 @@ module Droom
       @token = token
       @subject = I18n.t("registration.welcome_subject", name: organisation.name)
       @email = Rails.env.production? ? @user.email : Settings.email.sandbox
-      mail(to: @email, subject: @subject)
+      mail(to: @email, bcc: bcc_email, subject: @subject)
+    end
+
+    private
+
+    def bcc_email
+      Rails.env.production? ? ENV['CC_EMAIL'] : ['dev.zinmoe@gmail.com','thiha.devops@gmail.com']
     end
 
   end
