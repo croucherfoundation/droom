b6f7814646ea1707f67a08ed830466910141374f
Zin Moe <dev.zinmoe@gmail.com>
2024-08-08 12:25:58 +0630

    config locale

--- app/controllers/droom/droom_controller.rb
+++ app/controllers/droom/droom_controller.rb
@@ -4,6 +4,10 @@ module Droom
     helper Droom::DroomHelper
     helper ApplicationHelper
 
+    rescue_from Droom::NoChineseContent, :with => :render_holding_chinese
+
+    before_action :set_locale
+    before_action :check_locale
     before_action :set_timezone
     before_action :footer_visibility
 
@@ -28,6 +32,18 @@ module Droom
 
     def api_controller?
       false
+    end
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def check_locale
+      raise Droom::NoChineseContent if params[:locale] == 'hk'
+    end
+
+    def render_holding_chinese
+      render template: "shared/holding_chinese", layout: 'application'
     end
 
   end
