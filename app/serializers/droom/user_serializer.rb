6c08723427e115e2ca87df04996fcaa0f942b460
yarzardev <yarzarminwai97@gmail.com>
2024-10-31 17:08:56 +0630

    Add: address_type_id in user address serializer

--- app/serializers/droom/user_serializer.rb
+++ app/serializers/droom/user_serializer.rb
@@ -98,7 +98,8 @@ class Droom::UserSerializer < ActiveModel::Serializer
       {
         id: a.id,
         address: a.address,
-        type: a.address_type&.name
+        type: a.address_type&.name,
+        address_type_id: a.address_type.id
       }
     end
   end
