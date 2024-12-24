1654dc4446b1ecfc9b5ccdd67dd5846238bb1fd3
yarzardev <yarzarminwai97@gmail.com>
2024-12-24 09:16:22 +0630

    Add: screening section in dashboard page

--- app/controllers/droom/dashboard_controller.rb
+++ app/controllers/droom/dashboard_controller.rb
@@ -6,7 +6,7 @@ module Droom
     def index
       authorize! :read, :dashboard
       @timezone_feature = FeatureFlag.enabled?('time-zone-feature', current_user)
-      render layout: Droom.dashboard_layout.to_s
+      render layout: Droom.centered_layout.to_s
     end
 
   end
