9a067448f96e0115eddac1bdd7ce428358c34cff
yarzardev <yarzarminwai97@gmail.com>
2025-02-20 20:25:09 +0630

    Update: new colour to palette

--- app/models/droom/concerns/png_convert.rb
+++ app/models/droom/concerns/png_convert.rb
@@ -4,7 +4,7 @@ require 'mini_magick'
 
 module Droom::Concerns::PngConvert
   extend ActiveSupport::Concern
-  COLORS = %w( #F6E36C #CE1A1B #8ED7E2 #FFAE00 #7AAEE5 #E14CA3 #EF652A #2065C5 #7E0091).freeze
+  COLORS = %w( #F6E36C #EE3A43 #8ED7E2 #FFAE00 #7AAEE5 #E14CA3 #EF652A #2065C5 #7E0091 #2AB252).freeze
 
   def attach_initials_image(user)
     return if user.given_name.blank? && user.family_name.blank?
