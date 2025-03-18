fd3aba9eb9994d971ecb2196a51b9eea0370bb64
Thi Ha <thiha.dve@gmail.com>
2025-03-18 14:52:48 +0900

    paper trail  add

--- app/controllers/droom/api/api_controller.rb
+++ app/controllers/droom/api/api_controller.rb
@@ -1,6 +1,7 @@
 module Droom::Api
   class ApiController < Droom::DroomController
     include Droom::Concerns::LocalApi
+    include Droom::Concerns::PaperTrailWhodunnit
 
     respond_to :json
     skip_before_action :verify_authenticity_token, raise: false
