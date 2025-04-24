4223250730fab02a9c8c170fb08ff164daace8f1
Thi Ha <thiha.dve@gmail.com>
2025-04-24 13:16:00 +0900

    paper trail log metadata

--- app/controllers/droom/concerns/paper_trail_whodunnit.rb
+++ app/controllers/droom/concerns/paper_trail_whodunnit.rb
@@ -7,14 +7,17 @@ module Droom::Concerns::PaperTrailWhodunnit
 
   included do
     before_action :set_paper_trail_whodunnit
-    before_action :info_for_paper_trail
+    before_action :set_paper_trail_info
   end
 
   def set_paper_trail_whodunnit
     PaperTrail.request.whodunnit = user_signed_in? ? current_user.uid : nil
   end
 
-  def info_for_paper_trail
-    { ip: request.remote_ip, user_agent: request.user_agent }
+  def set_paper_trail_info
+    PaperTrail.request.controller_info = {
+      ip: request.remote_ip,
+      user_agent: request.user_agent
+  }
   end
 end
