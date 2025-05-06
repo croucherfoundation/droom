32619f8b7bbbec80a15202303a54c4527a520ca0
Zin Moe <dev.zinmoe@gmail.com>
2025-05-06 10:57:21 +0630

    auto generate compile PDF cover text

--- app/models/droom/event_type.rb
+++ app/models/droom/event_type.rb
@@ -17,7 +17,7 @@ module Droom
     def self.for_selection
       self.all.map {|et| [et.name, et.id]}
     end
-    
+
     def self.default
       default_type = self.where(:slug => "other_events").first_or_initialize
       if default_type.new_record?
@@ -29,6 +29,18 @@ module Droom
 
     def confidential?
       private?
+    end
+
+    def cover_text_lookup
+      {
+        2 => "An Investment Committee Meeting is to be held on",
+        3 => "An Audit Committee Meeting is to be held on",
+        6 => "A meeting of the CF Nomination & Remuneration Committee is to be held on",
+        9 => "A Trustees’ Meeting is to be held on",
+        10 => "A Governors’ Meeting is to be held on",
+        11 => "A meeting of the Academic Assessment Working Group is to be held on",
+        12 => "A meeting of the NCF Nomination Committee is to be held on"
+      }
     end
 
     protected
