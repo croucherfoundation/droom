ffc6269a4cffa0490e94e6a1328ba5282275df68
Thi Ha <thiha.dve@gmail.com>
2025-05-27 16:25:07 +0900

    Origin header fixed

--- app/controllers/droom/concerns/controller_helpers.rb
+++ app/controllers/droom/concerns/controller_helpers.rb
@@ -26,7 +26,7 @@ module Droom::Concerns::ControllerHelpers
 
     before_action :note_current_user, except: [:cors_check]
     before_action :set_section, except: [:cors_check]
-    before_action :set_access_control_headers
+    # before_action :set_access_control_headers
 
     skip_before_action :verify_authenticity_token, only: [:cors_check], raise: false
     after_action :update_auth_cookie, except: [:cors_check], unless: :api_controller?
