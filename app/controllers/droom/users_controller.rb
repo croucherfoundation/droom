021e26d323a5c84c675eb8b81a42d53f6be1f154
Zin Moe <dev.zinmoe@gmail.com>
2025-08-05 15:01:56 +0630

    remove bypass

--- app/controllers/droom/users_controller.rb
+++ app/controllers/droom/users_controller.rb
@@ -131,7 +131,7 @@ module Droom
       end
       current_user.assign_attributes(setup_params.merge(confirmed: true))
       if current_user.save
-        sign_in current_user, :bypass => true
+        sign_in current_user
         if current_user.data_room_user? || params[:send_invitation_memo].present?
           flash[:notice] = t(:password_set)
           redirect_to params[:destination].presence || droom.dashboard_url
