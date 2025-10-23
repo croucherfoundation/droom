1a5cc0d41f0862013a22429eed20786a74a5f0b6
kaungmyat <kmyatthu157@gmail.com>
2025-10-23 11:37:47 +0630

    remove unnecessary

--- app/controllers/droom/api/users_controller.rb
+++ app/controllers/droom/api/users_controller.rb
@@ -37,7 +37,6 @@ module Droom::Api
     #
     def authenticable
       @user.ensure_unique_session_id!
-      @user.reload
       render json: @user, serializer: Droom::UserAuthSerializer
     end
 
