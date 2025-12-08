6901f16aaf9482dd7214acdde63da35c4c75d9c8
Zin Moe <dev.zinmoe@gmail.com>
2025-12-08 11:08:11 +0630

    update subscrbe route structure

--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,7 @@ Droom::Engine.routes.draw do
     collection do
       get :calendar
       get :past
-      get "subscribe/:tok", action: "subscribe", as: :subscribe
+      get "subscribe(/:source)/:tok", action: "subscribe", as: :subscribe
     end
     resources :invitations do
       member do
