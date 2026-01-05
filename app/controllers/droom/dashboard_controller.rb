ff1dd40026c99b33b9fa524392cd4cafad41a2a0
Zin Moe <dev.zinmoe@gmail.com>
2026-01-05 14:57:48 +0630

    prepare summary for welcome page

--- app/controllers/droom/dashboard_controller.rb
+++ app/controllers/droom/dashboard_controller.rb
@@ -3,11 +3,28 @@ module Droom
     respond_to :html, :js
     skip_authorization_check
 
+    before_action :prepare_summary, only: [:index]
+
     def index
       authorize! :read, :dashboard
       @timezone_feature = FeatureFlag.enabled?('time-zone-feature', current_user)
       render layout: Droom.centered_layout.to_s
     end
 
+    private
+
+    def prepare_summary
+      grants = Grant.where(show: 1)
+      @total_grants = grants.total_count
+
+      awards = Award.where(show: 1)
+      @total_awards = awards.total_count
+
+      scholars = PersonPage.where(metadata: true)
+      @total_scholars = scholars.total_count
+
+      latest_fss_applications = Application.where(latest_fss: true)
+      @total_fss_applications = latest_fss_applications.total_count
+    end
   end
 end
