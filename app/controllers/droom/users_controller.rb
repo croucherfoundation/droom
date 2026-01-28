416925486f3d53bc1976d092d05e9932cc25b321
kaungmyat <kmyatthu157@gmail.com>
2026-01-28 09:55:07 +0630

    Removes unused layout selection logic from user controller

--- app/controllers/droom/users_controller.rb
+++ app/controllers/droom/users_controller.rb
@@ -328,13 +328,5 @@ module Droom
     def find_user_by_user_id
       @user ||= Droom::User.find_by_id(params[:user_id])
     end
-
-    def default_layout
-      if action_name == 'show'
-        'centered'
-      else
-        Droom.config.layout
-      end
-    end
   end
 end
