86370923f1fe5e5be36a80833fc5f506e9bd2629
Zin Moe <dev.zinmoe@gmail.com>
2026-03-16 17:40:14 +0630

    Update password toggle button class to 'eye-icon' in user forms for consistency

--- app/assets/javascripts/droom.js
+++ app/assets/javascripts/droom.js
@@ -201,7 +201,7 @@
         this.find_including_self('.sortable_folders').sortable_folders();
       }
       this.find_including_self('form.user.custom-validate').custom_validation();
-      this.find_including_self('form.user button.eye').password_toggle();
+      this.find_including_self('form.user button.eye-icon').password_toggle();
       this.find_including_self('[data-draggable]').draggable();
       this.find_including_self('.gridbox:not(.notice)').gridBox();
       this.find_including_self('.tagger').tagger();
