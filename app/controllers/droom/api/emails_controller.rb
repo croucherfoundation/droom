c42973f35f25380a63efaa85fad99ff98f310fd1
kaungmyat <kmyatthu157@gmail.com>
2026-05-11 16:27:57 +0630

    Add create action to EmailsController and update routes to support email creation

--- app/controllers/droom/api/emails_controller.rb
+++ app/controllers/droom/api/emails_controller.rb
@@ -1,6 +1,6 @@
 module Droom::Api
   class EmailsController < Droom::Api::ApiController
-    load_resource class: "Droom::Email"
+    load_resource class: "Droom::Email", only: [:show, :index]
 
     def index
       if params[:email]
@@ -15,5 +15,19 @@ module Droom::Api
       render json: @email, serializer: Droom::EmailSerializer
     end
 
+    def create
+      @email = Droom::Email.new(email_params)
+      if @email.save
+        render json: @email, serializer: Droom::EmailSerializer, status: :created
+      else
+        render json: { errors: @email.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def email_params
+      params.permit(:user_id, :email, :pending_email, :email_verification_token, :email_verification_sent_at, :address_type_id)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Droom::Engine.routes.draw do
     #post '/reindex_user' => 'users#reindex_user', as: 'reindex'
     #post '/users/:uid/reindex' => 'users#reindex', as: 'reindex'
     resources :addresses
-    resources :emails, only: [:show, :index]
+    resources :emails, only: [:show, :index, :create]
     resources :users do
       post 'reindex', on: :member, as: :reindex
       put 'update_contact',  on: :member, as: :update_contact
@@ -27,7 +27,7 @@ Droom::Engine.routes.draw do
       delete 'remove_reviewer_group', on: :member, as: :remove_reviewer_group
     end
     put "update_timezone" => 'users#update_timezone', as: 'update_timezone'
-    resources :events do 
+    resources :events do
       collection do
         get :calendar
       end
