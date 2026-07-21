3aec6ec326e50dc5994a3d313c9c94b13ad33de7
Zin Moe <dev.zinmoe@gmail.com>
2026-07-21 12:37:31 +0630

    Add ApiResponseHelper and FlashMessageHelper to controllers

--- app/controllers/droom/api/api_controller.rb
+++ app/controllers/droom/api/api_controller.rb
@@ -2,6 +2,7 @@ module Droom::Api
   class ApiController < Droom::DroomController
     include Droom::Concerns::LocalApi
     include Droom::Concerns::PaperTrailWhodunnit
+    include ApiResponseHelper
 
     respond_to :json
     skip_before_action :verify_authenticity_token, raise: false
--- app/controllers/droom/droom_controller.rb
+++ app/controllers/droom/droom_controller.rb
@@ -4,6 +4,8 @@ module Droom
     include Droom::Concerns::PaperTrailWhodunnit
     helper Droom::DroomHelper
     helper ApplicationHelper
+    include LocaleDetection
+    include FlashMessageHelper
 
 
     rescue_from Droom::NoChineseContent, :with => :render_holding_chinese
@@ -34,14 +36,6 @@ module Droom
 
     def api_controller?
       false
-    end
-
-    def set_locale
-      I18n.locale = params[:locale] || I18n.default_locale
-    end
-
-    def check_locale
-      raise Droom::NoChineseContent if params[:locale] == 'hk'
     end
 
     def render_holding_chinese
